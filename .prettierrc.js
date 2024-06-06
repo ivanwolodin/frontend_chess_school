0eddad22d7f34382c847bafc017c1f2462bbc4d9
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-06 19:13:03 +0500

    style: identation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	semi: false,
-	trailingComma: 'es5',
-	singleQuote: true,
-	jsxSingleQuote: true,
-	printWidth: 80,
-	useTabs: true,
-	tabWidth: 2,
-	endOfLine: 'lf',
-}
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  useTabs: true,
+  tabWidth: 4,
+  endOfLine: 'lf',
+};
