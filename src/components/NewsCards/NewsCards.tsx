e442f238674305707c7879a95133fde696b9d383
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-10 22:08:43 +0500

    feat(slider news): resizing on windows change

--- src/components/NewsCards/NewsCards.tsx
+++ src/components/NewsCards/NewsCards.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './NewsCards.css';
 import NewsCard from '../NewsCard/NewsCard';
 import { newsData } from '../../utils/constants';
+import { useState, useEffect } from 'react';
 
 export default function NewsCards() {
+  const [slidesToShow, setSlidesToShow] = useState(3); 
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth < 1024) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(3);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const settings = {
     dots: true,
-    infinite: false, // Включите бесконечный цикл
+    infinite: false,
     dotsClass: 'slick-dots slick-thumb',
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow,
+    slidesToScroll: 1,
     prevArrow: <button className="slick-prev">&#10094;</button>,
     nextArrow: <button className="slick-next">&#10095;</button>,
   };
