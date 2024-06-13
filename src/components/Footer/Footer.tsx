00956175fd95b27077c7da02b19825c1d3c35cc2
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-13 15:37:27 +0500

    style(footer): adjust size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -46,6 +46,47 @@ export default function Footer() {
         </div>
       </div>
       <hr></hr>
+      <div className="footer__second_section">
+        <div className="footer__icons">
+          <div className="footer__admins_icon">
+            <img
+              className="footer__perm_icon"
+              src={require('../../assets/icons/perm_department.png')}
+              alt="Департамент Перми иконка"
+            />
+            <img
+              className="footer__rcf_icon"
+              src={require('../../assets/icons/rcf_icon.jpeg')}
+              alt="РШФ иконка"
+            />
+          </div>
+        </div>
+        <div className="footer__warnings">
+          <p>
+            {' '}
+            Все материалы являются авторскими, любое использование разрешено
+            только с письменного согласия администрации сайта
+            www.шахматыпермь.рф
+          </p>
+          <br />
+          <p>
+            {' '}
+            Посещая страницы Сайта, заполняя формы ввода данных, пользователь
+            выражает своё согласие с Политикой конфиденциальности.
+          </p>
+          <br />
+          <p>
+            {' '}
+            © 2016 - 2024 <br />
+            ШАХМАТНАЯ ШКОЛА
+          </p>
+        </div>
+        <img
+          className="footer__visa_icon"
+          src={require('../../assets/icons/icons_cards.png')}
+          alt="vk"
+        />
+      </div>
     </footer>
   );
 }
