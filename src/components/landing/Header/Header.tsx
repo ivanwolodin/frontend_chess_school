449d7730e4bac8941c79d80459a9a49d7d6aae9a
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-15 19:02:16 +0500

    refactor(header): hardcoded <li/>

--- src/components/landing/Header/Header.tsx
+++ src/components/landing/Header/Header.tsx
@@ -1,16 +1,16 @@
-import { headerItems } from '../../../utils/constants';
-
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="header__general">
       <ul className="header__list">
-        {headerItems.map((item, index) => (
-          <li key={index} className="header__item">
-            {item}
-          </li>
-        ))}
+        <li className="header__item">ГЛАВНАЯ </li>
+        <li className="header__item">НОВОСТИ </li>
+        <li className="header__item">СОРЕВНОВАНИЯ </li>
+        <li className="header__item">О ШКОЛЕ </li>
+        <li className="header__item">ГРАФИК</li>
+        <li className="header__item">ПОСТУПЛЕНИЕ</li>
+        <li className="header__item">ЛИЧНЫЙ КАБИНЕТ </li>
       </ul>
     </header>
   );
