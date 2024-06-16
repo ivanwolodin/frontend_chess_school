095059abfcaaa22819612c2cd20566b180aef41e
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-16 14:19:30 +0500

    Updates

--- src/components/landing/HorizontalLine/HorizontalLine.tsx
+++
@@ -1,7 +0,0 @@
-import './HorizontalLine.css';
-
-const HorizontalLine = () => {
-  return <section className="horizontal_line"></section>;
-};
-
-export default HorizontalLine;
--- src/components/landing/InfoTinyCard/InfoTinyCard.tsx
+++
@@ -1,8 +0,0 @@
-import './InfoTinyCard.css';
-import { InfoTinyCardProps } from '../../../utils/interfaces';
-
-const InfoTinyCard = ({ src, alt }: InfoTinyCardProps) => {
-  return <img className="infotiny__card" src={src} alt={alt} />;
-};
-
-export default InfoTinyCard;
