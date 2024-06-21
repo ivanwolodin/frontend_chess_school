6658b3e659ac1c0f75d00822200c00a9337d8c6e
Ivan Volodin <ivanwolodin@gmail.com>
2024-06-21 20:18:04 +0500

    feat(attendance): implemented half

--- src/components/personal_account/TeacherPage/ManageAttendance/ManageAttendance.tsx
+++ src/components/personal_account/TeacherPage/ManageAttendance/ManageAttendance.tsx
@@ -1,74 +1,35 @@
-import React, { useState, useRef } from 'react';
 import './ManageAttendance.css';
 
 const ManageAttendance: React.FC = () => {
-  const [width, setWidth] = useState<number>(400); // Изначальная ширина
-  const [groupTitle, setGroupTitle] = useState<string>(''); // Заголовок группы
-  const containerRef = useRef<HTMLDivElement>(null);
-
   const items = [
-    'Группа 1',
-    'Группа 2',
-    'Группа 3',
-    'Группа 4',
-    'Группа 5',
-    'Группа 6',
-    'Группа 7',
-    'Группа 8',
-    'Группа 9',
-    'Группа 10',
+    'АТ11',
+    'АТ12',
+    'АТ13',
+    'СпецВТЧТ',
+    'АТВтЧт1',
+    'АТВтЧт16',
+    'АТ_ПТ14',
+    'АТ_ПТ16',
+    'СпецВТЧТ',
+    'СпецВТЧТ',
   ];
-
-  const handleMouseDown = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    const startX = e.clientX;
-
-    const handleMouseMove = (e: MouseEvent) => {
-      const newWidth = width + (e.clientX - startX);
-      setWidth(newWidth);
-    };
-
-    const handleMouseUp = () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-  };
-
-  const handleItemClick = (item: string) => {
-    setGroupTitle(item);
-  };
 
   return (
     <div className="manageattendance__general">
-      <div
-        ref={containerRef}
-        className="manageattendance__container"
-        style={{ width: `${width}px` }}
-      >
-        <div className="manageattendance__title">Список групп</div>
-        <div className="manageattendance__group_list">
+      <div className="manageattendance__main_block">
+        <h2 className="manageattendance__title">Группы: </h2>
+        <ul className="manageattendance__group_list">
           {items.map((item, index) => (
-            <button
-              key={index}
-              className="manageattendance__group_item"
-              onClick={() => handleItemClick(item)}
-            >
+            <li key={index} className="manageattendance__group_item">
               {item}
-            </button>
+            </li>
           ))}
+        </ul>
+        <div className="manageattendance__month_controls">
+          <div className="manageattendance__month_controls_leftarrow"></div>
+          <p className="manageattendance__month_controls_monthtitle">Июнь</p>
+          <div className="manageattendance__month_controls_rightarrow"></div>
         </div>
-        <button
-          className="manageattendance__vertical_line"
-          onMouseDown={handleMouseDown}
-        ></button>
-      </div>
-      <div className="manageattendance__details">
-        <div className="manageattendance__group_title">{groupTitle}</div>
-        {/* Здесь можно добавить таблицу или другие детали */}
       </div>
     </div>
   );
