ebf5f34744cfc4629a876a5a6e0232c980ac3a18
Ivan Volodin <ivanwolodin@gmail.com>
2024-07-10 22:48:12 +0500

    Add: func

--- src/components/landing/Header/Header.tsx
+++ src/components/landing/Header/Header.tsx
@@ -1,19 +1,30 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header__general">
-      <ul className="header__list">
+      <button className="header__toggle" onClick={toggleMenu}>
+        &#x2630; МЕНЮ
+      </button>
+      <ul className={`header__list ${isMenuOpen ? 'header__list_open' : ''}`}>
         <li className="header__item">
           <Link className="header__link" to="/">
             ГЛАВНАЯ
           </Link>
         </li>
-        <li className="header__item">НОВОСТИ </li>
-        <li className="header__item">СОРЕВНОВАНИЯ </li>
-        <li className="header__item">О ШКОЛЕ </li>
+        <li className="header__item">НОВОСТИ</li>
+        <li className="header__item">СОРЕВНОВАНИЯ</li>
+        <li className="header__item">О ШКОЛЕ</li>
         <li className="header__item">График</li>
         <li className="header__item">ПОСТУПЛЕНИЕ</li>
         <li className="header__item">
