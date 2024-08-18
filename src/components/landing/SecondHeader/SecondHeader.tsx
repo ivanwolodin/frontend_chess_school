f3dd90c30d5fc6352a880c249a29bda0525e466e
Ivan Volodin <ivanwolodin@gmail.com>
2024-08-18 11:26:00 +0500

    style: changed icon arrangement

--- src/components/landing/SecondHeader/SecondHeader.tsx
+++ src/components/landing/SecondHeader/SecondHeader.tsx
@@ -16,24 +16,6 @@ const SecondHeader = () => {
     <section className="second-header__general">
       <div className="second-header__icons">
         <a
-          href="https://vk.com/chessschool59"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/vk_i.png')}
-            alt="VK"
-          />
-        </a>
-        <a href="tel:+7342293006">
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/phone_i.png')}
-            alt="Phone"
-          />
-        </a>
-        <a
           href="https://www.youtube.com/channel/UCiTDxF0Ab3rMLdGYFFejWrw/featured"
           target="_blank"
           rel="noopener noreferrer"
@@ -41,7 +23,28 @@ const SecondHeader = () => {
           <img
             className="second-header__icon second-header__icon_y"
             src={require('../../../assets/icons/y.png')}
-            alt="Yandex"
+            alt="YouTube"
+          />
+        </a>
+        <a href="mailto:chessperm@yandex.ru">
+          <img
+            className="second-header__icon"
+            src={require('../../../assets/icons/mail_i.png')}
+            alt="Email"
+          />
+        </a>
+        <a href="https://invite.viber.com/?g2=AQAbJcB6hTfkBEp%2BEofyabJtKo%2BbdNfEFMf%2FjBrCctwSX%2BFC1WUfwq7eKNLM%2B7nO&lang=ru">
+          <img
+            className="second-header__icon"
+            src={require('../../../assets/icons/viber_i.png')}
+            alt="Viber"
+          />
+        </a>
+        <a href="tel:+7342293006">
+          <img
+            className="second-header__icon"
+            src={require('../../../assets/icons/phone_i.png')}
+            alt="Phone"
           />
         </a>
         <a
@@ -55,18 +58,15 @@ const SecondHeader = () => {
             alt="Telegram"
           />
         </a>
-        <a href="https://invite.viber.com/?g2=AQAbJcB6hTfkBEp%2BEofyabJtKo%2BbdNfEFMf%2FjBrCctwSX%2BFC1WUfwq7eKNLM%2B7nO&lang=ru">
+        <a
+          href="https://vk.com/chessschool59"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             className="second-header__icon"
-            src={require('../../../assets/icons/viber_i.png')}
-            alt="Viber"
-          />
-        </a>
-        <a href="mailto:chessperm@yandex.ru">
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/mail_i.png')}
-            alt="Email"
+            src={require('../../../assets/icons/vk_i.png')}
+            alt="VK"
           />
         </a>
       </div>
