d094492677f569104e28c26222452c1d2442a82d
Ivan Volodin <ivanwolodin@gmail.com>
2024-08-31 20:10:22 +0500

    feat: added: new info

--- src/components/landing/SecondHeader/SecondHeader.tsx
+++ src/components/landing/SecondHeader/SecondHeader.tsx
@@ -16,61 +16,64 @@ const SecondHeader = () => {
 
   return (
     <section className="second-header__general">
-      <div className="second-header__icons">
-        <a
-          href="https://www.youtube.com/channel/UCiTDxF0Ab3rMLdGYFFejWrw/featured"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="second-header__icon second-header__icon_y"
-            src={require('../../../assets/icons/y.png')}
-            alt="YouTube"
-          />
-        </a>
-        <a href="mailto:chessperm@yandex.ru">
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/mail_i.png')}
-            alt="Email"
-          />
-        </a>
-        <a href="https://invite.viber.com/?g2=AQAbJcB6hTfkBEp%2BEofyabJtKo%2BbdNfEFMf%2FjBrCctwSX%2BFC1WUfwq7eKNLM%2B7nO&lang=ru">
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/viber_i.png')}
-            alt="Viber"
-          />
-        </a>
-        <a href="tel:+73422393006">
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/phone_i.png')}
-            alt="Phone"
-          />
-        </a>
-        <a
-          href="https://t.me/chessschool59"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/tg_i.png')}
-            alt="Telegram"
-          />
-        </a>
-        <a
-          href="https://vk.com/chessschool59"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="second-header__icon"
-            src={require('../../../assets/icons/vk_i.png')}
-            alt="VK"
-          />
-        </a>
+      <div className="second-header__nav_info">
+        <p className="second-header__address">г. Пермь, ул. Куйбышева, 54</p>
+        <div className="second-header__icons">
+          <a
+            href="https://www.youtube.com/channel/UCiTDxF0Ab3rMLdGYFFejWrw/featured"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="second-header__icon second-header__icon_y"
+              src={require('../../../assets/icons/y.png')}
+              alt="YouTube"
+            />
+          </a>
+          <a href="mailto:chessperm@yandex.ru">
+            <img
+              className="second-header__icon"
+              src={require('../../../assets/icons/mail_i.png')}
+              alt="Email"
+            />
+          </a>
+          <a href="https://invite.viber.com/?g2=AQAbJcB6hTfkBEp%2BEofyabJtKo%2BbdNfEFMf%2FjBrCctwSX%2BFC1WUfwq7eKNLM%2B7nO&lang=ru">
+            <img
+              className="second-header__icon"
+              src={require('../../../assets/icons/viber_i.png')}
+              alt="Viber"
+            />
+          </a>
+          <a href="tel:+73422393006">
+            <img
+              className="second-header__icon"
+              src={require('../../../assets/icons/phone_i.png')}
+              alt="Phone"
+            />
+          </a>
+          <a
+            href="https://t.me/chessschool59"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="second-header__icon"
+              src={require('../../../assets/icons/tg_i.png')}
+              alt="Telegram"
+            />
+          </a>
+          <a
+            href="https://vk.com/chessschool59"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="second-header__icon"
+              src={require('../../../assets/icons/vk_i.png')}
+              alt="VK"
+            />
+          </a>
+        </div>
       </div>
       <img className="second-header__logo" src={logo} alt="logo"></img>
       <div className="second-header__quotation">{randomQuote}</div>
